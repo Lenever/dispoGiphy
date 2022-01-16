3e6d8b45a0cde2555bb8968c636c2fd31fda80fe
Ikechukwu Onuorah <onuorah@coffeit.nl>
2022-01-16 22:40:26 +0100

    add api key

--- Dispo Take Home/Util/Constants.swift
+++ Dispo Take Home/Util/Constants.swift
@@ -2,7 +2,7 @@ enum Constants {}
 
 extension Constants {
   // Get an API key from https://developers.giphy.com/dashboard/
-  static let giphyApiKey = ""
+  static let giphyApiKey = "hbOoZFC6ZBQh2mKhy5ILBP5MuWB2Ujbe"
     
   static let customCellIdentifier = "customCellIdentifier"
   static let fatalError = "Could not deque cell"
